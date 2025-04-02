5b98f4edce2606a708e87f4bbb10d9cdd2c71bd9
Matthew Stibbins <matthewstibbins@gmail.com>
2025-04-02 12:47:37 -0700

    add initial project files

---
+++ src/main/java/matt/calculateaim/CalculateAimApplication.java
@@ -0,0 +1,13 @@
+package matt.calculateaim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateAimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculateAimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/matt/calculateaim/CalculateAimApplicationTests.java
@@ -0,0 +1,13 @@
+package matt.calculateaim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateAimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
