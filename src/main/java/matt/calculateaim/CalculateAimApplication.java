87671f6b3f949c6b59f3dbaba9b1226b409e8074
Matthew Stibbins <matthewstibbins@gmail.com>
2025-04-02 12:56:26 -0700

    implement CommandLineRunner

--- src/main/java/matt/calculateaim/CalculateAimApplication.java
+++ src/main/java/matt/calculateaim/CalculateAimApplication.java
@@ -1,13 +1,20 @@
 package matt.calculateaim;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalculateAimApplication {
+public class CalculateAimApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CalculateAimApplication.class, args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("explicitly exiting since spring boot will not automatically exit");
+		System.exit(0);
+	}
 
 }
