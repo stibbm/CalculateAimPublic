5d3a7aa1d87d9562da6626bb8fb8f197caf241d0
Matthew Stibbins <matthewstibbins@gmail.com>
2025-04-02 15:01:57 -0700

    add CalculateAimProgram to be main program runner starting point

---
+++ src/main/java/matt/calculateaim/main/CalculateAimProgram.java
@@ -0,0 +1,17 @@
+package matt.calculateaim.main;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CalculateAimProgram {
+	public CalculateAimProgram() {
+
+	}
+
+	public void run(
+		String inputVideoFilePathString,
+		String outputDirectoryPathString
+	) {
+		
+	}
+}
