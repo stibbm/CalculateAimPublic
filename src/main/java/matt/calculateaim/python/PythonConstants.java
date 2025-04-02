c81b04de9c782ed4ba5ab90bde733e1c06286ec9
Matthew Stibbins <matthewstibbins@gmail.com>
2025-04-02 14:32:03 -0700

    add python constants

---
+++ src/main/java/matt/calculateaim/python/PythonConstants.java
@@ -0,0 +1,6 @@
+package matt.calculateaim.python;
+
+public class PythonConstants {
+	public static final String PYTHON_INTERPRETER = "python3.11";
+	public static final String PYTHON_DIRECTORY_PATH_STRING = "pythonfiles";
+}
